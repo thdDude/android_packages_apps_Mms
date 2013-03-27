556efd31dba2ae351f9d6bfb9d24073c9aa8c06a
Lars Greiss <kufikugel@googlemail.com>
2013-03-27 21:27:39 +0100

    Mms TRDS: Respect themes
    kinda strange situation....that we removed the
    whole CM theme toggle was right but causes really
    damn problems with themes. So to provide both
    our system and the one from CM......a bit magic
    
    Here the change
    
    Change-Id: Ic4a66a79bfc08d52acf30d53dd57755cb2ffac8f
    
    Conflicts:
    	res/values/colors.xml

--- src/com/android/mms/quickmessage/QuickMessagePopup.java
+++ src/com/android/mms/quickmessage/QuickMessagePopup.java
@@ -37,6 +37,7 @@ import android.os.Parcelable;
 import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract.Profile;
+import android.provider.Settings;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.InputFilter;
@@ -151,6 +152,7 @@ public class QuickMessagePopup extends Activity implements
     // Configuration
     private boolean mCloseClosesAll = false;
     private boolean mWakeAndUnlock = false;
+    private boolean mDarkTheme = false;
     private boolean mFullTimestamp = false;
     private int mUnicodeStripping = MessagingPreferenceActivity.UNICODE_STRIPPING_LEAVE_INTACT;
     private UnicodeFilter mUnicodeFilter = null;
@@ -194,6 +196,9 @@ public class QuickMessagePopup extends Activity implements
         mInputMethod = Integer.parseInt(prefs.getString(MessagingPreferenceActivity.INPUT_TYPE,
                 Integer.toString(InputType.TYPE_TEXT_VARIATION_SHORT_MESSAGE)));
 
+        mDarkTheme = Settings.Secure.getInt(mContext.getContentResolver(),
+                            Settings.Secure.UI_INVERTED_MODE, 0) == 1;
+
         // Set the window features and layout
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.dialog_quickmessage);
@@ -216,7 +221,11 @@ public class QuickMessagePopup extends Activity implements
 
         // Set the theme color on the pager arrow
         Resources res = getResources();
-        mQmPagerArrow.setBackgroundColor(res.getColor(R.color.quickmessage_body_light_bg));
+        if (mDarkTheme) {
+            mQmPagerArrow.setBackgroundColor(res.getColor(R.color.quickmessage_body_dark_bg));
+        } else {
+            mQmPagerArrow.setBackgroundColor(res.getColor(R.color.quickmessage_body_light_bg));
+        }
 
         // ViewPager Support
         mPagerAdapter = new MessagePagerAdapter();
@@ -951,7 +960,12 @@ public class QuickMessagePopup extends Activity implements
 
             // Load the layout to be used
             LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View layout = inflater.inflate(R.layout.quickmessage_content_light, null);
+            View layout;
+            if (mDarkTheme) {
+                layout = inflater.inflate(R.layout.quickmessage_content_dark, null);
+            } else {
+                layout = inflater.inflate(R.layout.quickmessage_content_light, null);
+            }
 
             // Load the main views
             EditText qmReplyText = (EditText) layout.findViewById(R.id.embedded_text_editor);
@@ -976,7 +990,11 @@ public class QuickMessagePopup extends Activity implements
                 updateContactBadge(qmContactBadge, qm.getFromNumber()[0], false);
                 qmMessageText.setText(formatMessage(qm.getMessageBody()));
 
-                qmReplyText.setBackgroundResource(R.drawable.edit_text_holo_light);
+                if (!mDarkTheme) {
+                    // We are using a holo.light background with a holo.dark activity theme
+                    // Override the EditText background to use the holo.light theme
+                    qmReplyText.setBackgroundResource(R.drawable.edit_text_holo_light);
+                }
 
                 // Set the remaining values
                 qmReplyText.setInputType(InputType.TYPE_CLASS_TEXT | mInputMethod
